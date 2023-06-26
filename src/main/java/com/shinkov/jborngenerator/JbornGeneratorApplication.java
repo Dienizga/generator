a1d3020696c8112a22172d237fdb640622729238
Dienizga <shinckov@yandex.ru>
2023-06-26 21:03:36 +0300

    2 commit

--- src/main/java/com/shinkov/jborngenerator/JbornGeneratorApplication.java
+++ src/main/java/com/shinkov/jborngenerator/JbornGeneratorApplication.java
@@ -1,13 +1,16 @@
 package com.shinkov.jborngenerator;
 
+import com.shinkov.jborngenerator.service.Producer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class JbornGeneratorApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(JbornGeneratorApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(JbornGeneratorApplication.class, args);
+        Producer producer = context.getBean(Producer.class);
+        producer.sendMessages();
     }
-
 }
--- src/main/java/com/shinkov/jborngenerator/service/Producer.java
+++ src/main/java/com/shinkov/jborngenerator/service/Producer.java
@@ -1,27 +1,39 @@
 package com.shinkov.jborngenerator.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
-@Slf4j
+import java.util.concurrent.ThreadLocalRandom;
+
 @Component
+@Slf4j
 @RequiredArgsConstructor
 public class Producer {
-    @Value("${topic.name}")
-    private String orderTopic;
-    private final ObjectMapper objectMapper;
-    private final KafkaTemplate<String, String> kafkaTemplate;
 
+    private final RandomWordsGeneratorService randomWordsGeneratorService;
+    private static Boolean toggle = true;
 
-    public String sendMessage(String str) throws JsonProcessingException {
-        String orderAsMessage = objectMapper.writeValueAsString(str);
-        kafkaTemplate.send(orderTopic, orderAsMessage);
-        log.info("Word is produced {}", orderAsMessage);
-        return "message sent";
+    @Value("${kafka.topics.topic}")
+    private String topic;
+
+    private int messageNumber = 0;
+
+    private final KafkaTemplate<Object, Object> kafkaTemplate;
+
+    public void sendMessages() {
+        try {
+            while (toggle) {
+                messageNumber++;
+                String word = randomWordsGeneratorService.generatorWord();
+                kafkaTemplate.send(topic, String.valueOf(ThreadLocalRandom.current().nextLong()), word);
+                log.info("Отправлено сообщение номер {}", messageNumber);
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
